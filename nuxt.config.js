bb239f50f1d4e1d0d1296b9c97f379f8c5c86b84
Adil Shehzad <adil.shehzad4146@gmail.com>
2021-07-27 18:51:43 +0000

    adding docs

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  docs: {
+    primaryColor: '#E24F55',
+    target: 'static',
+    router: {
+      base: 'https://adilshehzad786.github.io/automation-docs'
+    }
+  }
+})
